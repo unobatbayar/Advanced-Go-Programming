9f7b644b844ae36d25736b8131dba194e3b2c06f
unobatbayar <uno@konple.com>
2019-09-18 05:06:53 +0100

    feat: first command function, version 0.0.0

--- README.md
+++ README.md
@@ -6,5 +6,10 @@ Simple CLI written in Go
 ```
 1. brew install go
 2. go run cli.go
+```
 
+For commands e.g
+
+```
+1. go run cli.go ns --host konple.com
 ```
--- cli.go
+++ cli.go
@@ -3,12 +3,43 @@ package main
 import (
 	"log"
 	"os"
+	"net"
+	"fmt"
 
 	"github.com/urfave/cli"
 )
 
 func main(){
-	err := cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "Command Line Interface Tool"
+	app.Usage = "Use for name server lookup, IP lookup and more."
+
+	flags := []cli.Flag{
+		cli.StringFlag{ 
+			Name: "host",
+			Value: "konple.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name: "ns",
+			Usage: "Look up the name server",
+			Flags: flags,
+			Action: func(c *cli.Context) error{
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil{
+					return err
+				}
+				for i := 0; i < len(ns); i++{
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil{
 		log.Fatal(err)
 	}
